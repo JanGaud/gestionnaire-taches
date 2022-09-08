769c4301b9dce35beace51792d7c98509c6dafd0
JanGaud <JanisGaudreault.jg@gmail.com>
2022-09-08 19:02:06 -0400

    restart avec modules
    Je recommence tranquillement en modularisant.

---
+++ Javascript-TP2/script.js/programmeInitial.js
@@ -0,0 +1,24 @@
+import { GestionTache } from "./GestionTache";
+
+    //Importation des variables universelles
+    const btnAjouter = document.getElementById("ok");
+    const boiteClones = document.getElementById("liste-taches");
+    const checkedButton = document.getElementById("checkbox");
+    const tache = document.getElementById("inputTache").value;
+    const btnSupprimer = document.getElementById("supprimer");
+    const gabarit = document.getElementById("gabarit-taches");
+
+    console.log(btnAjouter);
+    console.log(boiteClones)
+    console.log(checkedButton)
+    console.log(tache)
+    console.log(btnSupprimer)
+    console.log(gabarit)
+    
+
+btnAjouter.addEventListener("click", AjouterTache);
+
+function AjouterTache(){
+    // const texteEntre = tache.value;
+    // new GestionTache(texteEntre, boiteClones, gabarit);
+}
