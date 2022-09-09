da3ab82416cba3d61307dce3c3777ca00850137e
JanGaud <JanisGaudreault.jg@gmail.com>
2022-09-08 21:38:32 -0400

    ajout de fonction dans le gestionnaire
    le clonage ne fonctionne quune seule fois par exemple.... :o

--- Javascript-TP2/script.js/gestionnaire.js
+++ Javascript-TP2/script.js/gestionnaire.js
@@ -22,12 +22,24 @@ export class GestionTache{
         this.cloneTexte = this.listeTaches.querySelector(".outputTache");
         this.cloneTexte.innerText = this.cloneTexte;
         this.checkbox = this.listeTaches.querySelector("input[type=checkbox]");
-        this.checkbox = this.listeTaches.querySelector("#checkbox")
+        this.checkbox = this.listeTaches.querySelector("#checkbox");
+        this.btnSupprimer = this.listeTaches.querySelector("#supprimer");
 
+
+        this.gabarit.classList.remove("invisible");
+
+
+        //event listener tache fait!
+
+        this.checkbox.addEventListener("click", function(){
+            that.Fait();
+        });
+
+
+        this.btnSupprimer.addEventListener("click",function delete()){
+            that.btnSupprimer();
+        }
 
     }
 
-    Supprimer() {
-        this.gabarit.remove();
-    }
 }
